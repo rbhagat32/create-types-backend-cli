717c914f049185111afd8db444311fea6ed7bd6f
rbhagat32 <raghavbhagat32@gmail.com>
2025-09-13 21:37:15 +0530

    rename to compose.yaml

--- src/services/docker.ts
+++ src/services/docker.ts
@@ -10,5 +10,5 @@ export const createDocker = (projectName: string, answers: Answers) => {
   fs.writeFileSync(".dockerignore", dockerIgnoreContent);
   fs.writeFileSync("Dockerfile", dockerfileContent(answers));
   fs.writeFileSync("Dockerfile.dev", dockerfileDevContent(answers));
-  fs.writeFileSync("docker-compose.yml", dockercomposeContent(projectName, answers));
+  fs.writeFileSync("compose.yaml", dockercomposeContent(projectName, answers));
 };
