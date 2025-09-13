f736981b3096f3117c4a681bf2e58a2d77461fb1
rbhagat32 <raghavbhagat32@gmail.com>
2025-09-13 21:55:40 +0530

    down command added in package.json

--- src/content/package-json.ts
+++ src/content/package-json.ts
@@ -3,15 +3,15 @@ export const packageJsonContent = (projectName: string, answers: Answers) => `{
   "version": "1.0.0",
   "private": true,
   "description": "This app was created using npx create-types-backend@latest",
-  "main": "dist/app.js",
   "type": "module",
+  "main": "dist/app.js",
   "scripts": {
     "start": "node dist/app.js",
     "build": "${answers.useESLint ? "npm run lint && " : ""}tsc && tsc-alias",
     "dev": "nodemon"${!answers.useESLint && !answers.useDocker ? "" : ","}
     ${answers.useESLint ? `"lint": "eslint src"${answers.useDocker ? "," : ""}` : ""}
     ${answers.useDocker ? '"up": "docker compose up -d --build && docker compose watch",' : ""}
-    ${answers.useDocker ? '"down": "docker-compose down"' : ""}
+    ${answers.useDocker ? '"down": "docker compose down"' : ""}
   },
   "keywords": [],
   "author": "",
